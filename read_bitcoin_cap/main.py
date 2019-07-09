a2dff7f0557259605ac05a1736a43fb557834d4e
ryugakune-rikuya <ev50516@meiji.ac.jp>
2019-07-09 18:07:56 +0900

    inputアドレスを出力できるように調整

--- read_bitcoin_cap/main.py
+++ read_bitcoin_cap/main.py
@@ -2,6 +2,7 @@ import pyshark
 
 from hashlib import *
 from base58 import *
+import subprocess
 
 file_path = '/Users/igakishuusei/Desktop/bitcoin.cap'
 file_path = '../../../03,データセット/bitcoin.cap'
@@ -39,22 +40,60 @@ def GetInputDataFromPacket(packet):
         print("- intput")
         input_count = int(packet["BITCOIN"].get_field_value('tx').get_field_value('input_count'))
         input_list = packet["BITCOIN"].get_field_value('tx').get_field_value('in')
-
         if input_count > 1:
             for num in range(input_count):
                 script = input_list[num].get_field_value("sig_script").replace(':','')
-                print(PubkeyToAddress(script[2:144]))
+                results = getPublicKeyFromBitcoinScript(script, True)
+                if type(results) is list:
+                    for result in results:
+                        result = getPublicKeyFromBitcoinScript(result.replace("[","").replace("]",""), True)
+                        if result == None:
+                            continue
+                        print(PubkeyToAddress(result))
+                elif results != None:
+                    print(PubkeyToAddress(results))
         elif input_count == 0:
             pass
         else:
             script = input_list.get_field_value("sig_script").replace(':','')
             if script != "None":
-                print(PubkeyToAddress(script[2:144]))
+                results = getPublicKeyFromBitcoinScript(script, True)
+                if type(results) is list:
+                    for result in results:
+                        result = getPublicKeyFromBitcoinScript(result.replace("[","").replace("]",""), True)
+                        if result == None:
+                            continue
+                        print(PubkeyToAddress(result))
+                elif results != None:
+                    print(PubkeyToAddress(results))
+
+def getPublicKeyFromBitcoinScript(script, input_flag=False):
+    result = subprocess.check_output(["bx", "script-decode", script]).decode().replace("\n","")
+    result_list = result.split(" ")
+    print(result_list)
+    if input_flag:
+        if result_list[-1] == "<invalid>":
+            return None
+        elif result_list[0] == "zero":
+            return result_list[1:]
+        else:
+            return result_list[1].replace("[","").replace("]","")
+    else:
+        if 'dup' == result_list[0] and 'hash160' == result_list[1]:
+            return result_list[2].replace("[","").replace("]","")
+        elif 'hash160' == result_list[0] and 'equal' == result_list[2]:
+            return result_list[1].replace("[","").replace("]","")
+        elif 'return' == result_list[0]:
+            return None
+        elif '<invalid>' == result_list[-1]:
+            return None
+        else:
+            return result_list
+
 
 def GetOutputDataFromPacket(packet):
     if packet["BITCOIN"].get_field_value('command') == "tx" and packet["BITCOIN"].get_field_value('tx').get_field_value('output_count') != "0":
         print("- output")
-        print(packet["BITCOIN"].get_field_value('tx'))
         input_count = int(packet["BITCOIN"].get_field_value('tx').get_field_value('input_count'))
         output_count = int(packet["BITCOIN"].get_field_value('tx').get_field_value('output_count'))
         output_list = packet["BITCOIN"].get_field_value('tx').get_field_value('out')
@@ -63,22 +102,38 @@ def GetOutputDataFromPacket(packet):
                 for num in range(output_count):
                     if output_list[num].has_field("script"):
                         script = output_list[num].get_field_value("script").replace(':','')
-                        print(ScriptToAddress("00"+script[6:46]))
+                        result = getPublicKeyFromBitcoinScript(script)
+                        if result != None:
+                            print(result)
+                            print(ScriptToAddress("00"+result))
+                            #print(ScriptToAddress("00"+script[6:46]))
             else:
                 if output_list.has_field("script"):
                     script = output_list.get_field_value("script").replace(':','')
-                    print(ScriptToAddress("00"+script[6:46]))
+                    result = getPublicKeyFromBitcoinScript(script)
+                    if result != None:
+                        print(result)
+                        print(ScriptToAddress("00"+result))
+                        #print(ScriptToAddress("00"+script[6:46]))
 
         else:
             if output_count > 1:
                 for num in range(output_count):
                     if output_list[num].has_field("script"):
                         script = output_list[num].get_field_value("script").replace(':','')
-                        print(ScriptToAddress("00"+script[6:46]))
+                        result = getPublicKeyFromBitcoinScript(script)
+                        if result != None:
+                            print(result)
+                            print(ScriptToAddress("00"+result))
+                            #print(ScriptToAddress("00"+script[6:46]))
             else:
                 if output_list.has_field("script"):
                     script = output_list.get_field_value("script").replace(':','')
-                    print(ScriptToAddress("00"+script[6:46]))
+                    result = getPublicKeyFromBitcoinScript(script)
+                    if result != None:
+                        print(result)
+                        print(ScriptToAddress("00"+result))
+                        #print(ScriptToAddress("00"+script[6:46]))
 
 def GetIpFromPacket(packet):
     if packet["IP"].get_field_value('src_host'):
@@ -97,8 +152,8 @@ def main():
             print("====================================")
             print("num of packet : "+str(num))
             num += 1
-            GetIpFromPacket(packet)
-            GetTcpFromPacket(packet)
+            #GetIpFromPacket(packet)
+            #GetTcpFromPacket(packet)
             GetInputDataFromPacket(packet)
             GetOutputDataFromPacket(packet)
 
