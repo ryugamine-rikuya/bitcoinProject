ecc69d63a7bc2038f796783e0cc3489bada37c07
ryugakune-rikuya <ev50516@meiji.ac.jp>
2019-09-01 09:22:51 -0400

    日毎の取引回数グラフを作成する関数を追加

--- get_tx_of_address/main.py
+++ get_tx_of_address/main.py
@@ -3,8 +3,12 @@ import logging
 from logging import getLogger, StreamHandler, Formatter, FileHandler
 from bs4 import BeautifulSoup
 import time
+import sys
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from datetime import datetime
+from datetime import timedelta
 
 # global var setting
 APP_NAME = "get_tx_of_address"
@@ -76,37 +80,82 @@ def get_address_data(address):
     logger.info(address)
     num_of_txs = get_number_of_txs(address)
     time_list = get_element_of_tx(address, num_of_txs)
-    return pd.DataFrame(time_list)
+    df_time = add_col_hh_to_df_time(pd.DataFrame(time_list))
+    df_time['YYYY-MM-DD'] = pd.to_datetime(df_time['YYYY-MM-DD'])
+    df_time.sort_values(by='YYYY-MM-DD', inplace=True)
+    df_time.reset_index(inplace=True, drop=True)
+    return df_time
+
+
+
+def add_col_hh_to_df_time(df_time):
+    df_time = pd.concat([df_time, df_time["time"].str.split(" ", n=2, expand=True)], axis=1)
+    df_time.rename(columns={0:"YYYY-MM-DD",1:"hh:mm:ss"},inplace=True)
+    df_time = pd.concat([df_time, df_time["YYYY-MM-DD"].str.split("-", n=2, expand=True)], axis=1)
+    df_time.rename(columns={0:"YYYY",1:"MM",2:"DD"},inplace=True)
+    df_time = pd.concat([df_time, df_time["hh:mm:ss"].str.split(":", n=2, expand=True)], axis=1)
+    df_time.rename(columns={0:"hh",1:"mm",2:"dd"},inplace=True)
+    df_time["hh"] = df_time.hh.astype('int32')
+    return df_time
 
 
 def insert_data():
     pass
 
-def make_graph_of_address(df, address, time_zone):
-    df = pd.concat([df, df["time"].str.split(" ", n=2, expand=True)], axis=1)
-    df.rename(columns={0:"YYYY-MM-DD",1:"hh:mm:ss"},inplace=True)
-    df = pd.concat([df, df["YYYY-MM-DD"].str.split("-", n=2, expand=True)], axis=1)
-    df.rename(columns={0:"YYYY",1:"MM",2:"DD"},inplace=True)
-    df = pd.concat([df, df["hh:mm:ss"].str.split(":", n=2, expand=True)], axis=1)
-    df.rename(columns={0:"hh",1:"mm",2:"dd"},inplace=True)
-    df["hh"] = df.hh.astype('int32')
+def make_graph_of_address_by_hour(df_time, address, time_zone):
     tx_count_list = []
     for i in range(24):
         time = (i - time_zone + 24) % 24
         tx_count_dict = {}
-        tx_count_dict["count"] = df[df.hh == time]["hh"].count()
+        tx_count_dict["count"] = df_time[df_time.hh == time]["hh"].count()
         tx_count_list.append(tx_count_dict)
-    count_df = pd.DataFrame(tx_count_list)
-    plt.plot(count_df["count"])
-    plt.savefig("{}.png".format(address))
-    return df
+    count_df_time = pd.DataFrame(tx_count_list)
+    plt.title("{}".format(address))
+    plt.xlabel("hour")
+    plt.ylabel("count")
+    plt.plot(count_df_time["count"])
+    plt.savefig("{}_by_hour.png".format(address))
+    plt.clf()
+
+def make_df_count_from_df_time(df_time):
+    first_day = df_time.iloc[0]['YYYY-MM-DD'] - timedelta(days=1)
+    last_day = df_time.iloc[-1]['YYYY-MM-DD'] + timedelta(days=2)
+    df_graph = df_time['YYYY-MM-DD'].value_counts(sort=True, ascending=True)
+    date_list = []
+    count_list = []
+    graph_date_list = df_graph.index.astype("str").tolist()
+    num_of_days = 0
+    while first_day != last_day:
+        date_list += [first_day.strftime("%Y-%m-%d")]
+        if first_day.strftime("%Y-%m-%d") in graph_date_list:
+            count_list.append(df_graph[first_day.strftime("%Y-%m-%d")])
+        else:
+            count_list.append(0)
+        first_day += timedelta(days=1)
+        num_of_days += 1
+    return pd.DataFrame(data={'date': date_list, 'count': count_list}, columns=['date', 'count']), df_time.iloc[0]['YYYY-MM-DD'].strftime("%Y-%m-%d %H:%M:%S"), num_of_days
+
+def make_graph_of_address_by_day(df_time, address, time_zone):
+    df_count, first_day, num_of_days = make_df_count_from_df_time(df_time)
+    df_count['date'] = pd.to_datetime(df_count['date'])
+    x1 = pd.date_range(first_day, periods=num_of_days,freq='d')
+    fig = plt.figure(figsize=(20,8))
+    ax = fig.add_subplot(1,1,1)
+    ax.plot(x1, df_count["count"])
+    ax.set_xlabel('date')
+    ax.set_ylabel('count')
+    ax.set_title('{}'.format(address))
+    plt.savefig("{}_by_days.png".format(address))
+    plt.clf()
+
 
 def main():
-    address = "35pJQef1CGscLec9jyddMu2DLU5Swq12wK"
-    address = "19vNKwvSHTvGYUtftUTe7haaTYLdk742Bm"
-    df = get_address_data(address)
+    args = sys.argv
     time_zone = -4
-    df = make_graph_of_address(df, address, time_zone)
+    address = args[1]
+    df_time = get_address_data(address)
+    make_graph_of_address_by_day(df_time, address, time_zone)
+    make_graph_of_address_by_hour(df_time, address, time_zone)
 
 if __name__ == "__main__":
     main()
